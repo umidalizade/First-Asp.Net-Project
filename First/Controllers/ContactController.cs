39fa63497ce57d7d0df2e1b92c86a1a14a30a69b
Umid Alizade <mr.umid.alizade@gmail.com>
2022-11-14 01:18:40 +0400

    AboutPage_ContactPage_WriterController_Authentication

---
+++ First/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrate;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace First.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager ct = new ContactManager(new EfContactRepository());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Contact p)
+        {
+            p.ContactDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.ContactStatus = true;
+            ct.ContactAdd(p);
+            return RedirectToAction("Index","Blog");
+        }
+    }
+}
---
+++ First/Controllers/WriterController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace First.Controllers
+{
+    [Authorize]
+    public class WriterController : Controller
+    {
+        //[Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
+    }
+}
